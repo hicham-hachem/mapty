96f6d61dcfef63530d56916e3d0e3450d3d8c07f
hicham <hicham.hachem1@gmail.com>
2022-12-01 00:51:52 +0200

    Move to marker on click

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 class Workout {
     date = new Date();
     id = (Date.now() + '').slice(-10);
+    clicks = 0;
 
     constructor(coords, distance, duration) {
         this.coords = coords; // [lat, lng]
@@ -19,6 +20,12 @@ class Workout {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
         // prettier-ignore
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
+
+    //************************************************//
+
+    click() {
+        this.clicks++;
     }
 }
 
@@ -83,6 +90,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 //  APPLICATION ARCHITECTURE
 class App {
     #map;
+    #mapZoomLevel = 13;
     #mapEvent;
     #workouts = [];
 
@@ -92,6 +100,10 @@ class App {
         form.addEventListener('submit', this._newWorkOut.bind(this));
         // Toggle between elevation and cadence
         inputType.addEventListener('change', this._toggleElevationField);
+        containerWorkouts.addEventListener(
+            'click',
+            this._moveToPopup.bind(this)
+        );
     }
 
     //************************************************//
@@ -116,7 +128,7 @@ class App {
 
         const coords = [latitude, longitude];
 
-        this.#map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution:
@@ -292,6 +304,26 @@ class App {
         }
 
         form.insertAdjacentHTML('afterend', html);
+    }
+
+    //************************************************//
+
+    _moveToPopup(e) {
+        const workoutEl = e.target.closest('.workout');
+
+        if (!workoutEl) return;
+
+        const workout = this.#workouts.find(
+            work => work.id === workoutEl.dataset.id
+        );
+
+        this.#map.setView(workout.coords, this.#mapZoomLevel, {
+            animate: true,
+            pan: { duration: 1 }
+        });
+
+        // Using the public interface
+        workout.click();
     }
 }
 
